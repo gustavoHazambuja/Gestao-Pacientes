098e48697e89a7a953f1a725a70b2457a068ba29
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-13 13:36:28 -0300

    entities add

---
+++ Gestao-Pacientes/src/main/java/Gestao_Pacientes/Entities/Paciente.java
@@ -0,0 +1,46 @@
+package Gestao_Pacientes.Entities;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_pacients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String CPF;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate dateBirth;
+
+    @Enumerated(EnumType.STRING)
+    private PacienteStatus status;
+    private Date timesTamp;
+}
---
+++ Gestao-Pacientes/src/main/java/Gestao_Pacientes/Entities/PacienteStatus.java
@@ -0,0 +1,9 @@
+package Gestao_Pacientes.Entities;
+
+public enum PacienteStatus {
+    AGUARDANDO_ATENDIMENTO,
+    EM_ATENDIMENTO,
+    ATENDIDO,
+    INTERNADO,
+    ÓBITO
+}
