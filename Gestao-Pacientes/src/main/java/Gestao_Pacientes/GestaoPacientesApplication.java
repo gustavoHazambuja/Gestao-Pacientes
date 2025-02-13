b3f96a667f871d58a10502a309c07db7b6c9ad3a
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-13 13:19:28 -0300

    fist commit

---
+++ Gestao-Pacientes/src/main/java/Gestao_Pacientes/GestaoPacientesApplication.java
@@ -0,0 +1,13 @@
+package Gestao_Pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPacientesApplication.class, args);
+	}
+
+}
---
+++ Gestao-Pacientes/src/test/java/Gestao_Pacientes/GestaoPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package Gestao_Pacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
