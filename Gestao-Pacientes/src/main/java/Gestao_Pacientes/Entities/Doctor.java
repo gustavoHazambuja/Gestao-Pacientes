5714d0b24a8333df96c3b26a637a831511f81422
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-13 22:34:03 -0300

    project finished

--- Gestao-Pacientes/src/main/java/Gestao_Pacientes/Entities/Doctor.java
+++ Gestao-Pacientes/src/main/java/Gestao_Pacientes/Entities/Doctor.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@NoArgsConstructor
 public class Doctor {
     
     @Id
